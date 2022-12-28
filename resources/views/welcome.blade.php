ff7c57aea0e92f24f3a532a319eae28f27002382
ZaynOssama <94825975+ZaynOssama@users.noreply.github.com>
2022-12-29 00:10:25 +0100

    Added links
    Added Bootstrap links to welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,11 +8,18 @@
 
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="">
-
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     </head>
     <body class="antialiased">
-        <div id="app"></div>
+        <header>
+            <div id="header"></div>
+        </header>
+        <div id="app" class="container"></div>
+
+        <footer>
+
+        </footer>
 
         @vite('resources/js/app.js')
     </body>
