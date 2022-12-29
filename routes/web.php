4063836a91d98091e392bd2e076000c3b95a7e8f
ZaynOssama <94825975+ZaynOssama@users.noreply.github.com>
2022-12-30 00:38:27 +0100

    Firsts Steps

--- routes/web.php
+++ routes/web.php
@@ -22,9 +22,9 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/welcome', function () {
-    return view('welcome');
-})->middleware(['auth', 'verified'])->name('welcome');
+Route::get('/zaynsworld', function () {
+    return view('index');
+})->middleware(['auth', 'verified'])->name('zaynsworld');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
