0767cb5d060b0fca3b6be8cf80d565de1225f895
ZaynOssama <94825975+ZaynOssama@users.noreply.github.com>
2022-12-30 00:16:58 +0100

    Components working 100%

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,9 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+import { createApp } from "vue";
+
+import App from "./layouts/App.vue"
+const app = createApp(App)
+app.mount("#app")
