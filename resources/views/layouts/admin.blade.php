ea0a509a9f084eac69bac922d0b35bdf6e5fd571
ZaynOssama <94825975+ZaynOssama@users.noreply.github.com>
2023-01-01 00:04:55 +0100

    working on the app interface

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link class="text-white" :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link class="text-white" :href="route('zaynsworld')" :active="request()->routeIs('zaynsworld')">
+                        {{ __('ZaynsWorld') }}
+                    </x-nav-link>
                 </div>
             </div>
 
