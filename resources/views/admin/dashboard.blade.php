33bfc4ad452a894d2fca13b1ac65d3b7337dd9d1
ZaynOssama <94825975+ZaynOssama@users.noreply.github.com>
2023-01-01 00:10:02 +0100

    new app component

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,4 +1,4 @@
-<x-appadmin-layout>
+<x-admin-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
@@ -14,4 +14,4 @@
             </div>
         </div>
     </div>
-</x-appadmin-layout>
+</x-admin-layout>
